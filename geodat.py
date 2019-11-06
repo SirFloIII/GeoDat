6908032fdacf6e7079e62f2bfff4d98899276742
SirFloIII <sirfloiii@gmail.com>
2019-11-06 21:03:42 +0100

    modified getPoints(). now prepends mueller url if given bare filename. useful.

--- geodat.py
+++ geodat.py
@@ -4,12 +4,19 @@ import urllib.request
 import os
 
 def getPoints(url,back="src"):
-    filename = os.path.join(back,os.path.basename(url))
+ 
+    if not "https" in url:
+        url = os.path.join("https://www.geometrie.tuwien.ac.at/mueller/files/daten/", url)
+    
+    filename = os.path.join(back, os.path.basename(url))
     if os.path.isfile(filename):
         points = np.loadtxt(filename)
         return points
+    
     if not os.path.exists(back):
         os.makedirs(back)
+    
     urllib.request.urlretrieve(url, filename)
     points = np.loadtxt(filename)
     return points
+
